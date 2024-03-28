3bdd7e41c1b4f25f115161ba3fe370348ef2cd1e
Noamakkusu <noahcgo@gmail.com>
2024-03-28 17:07:25 -0400

    about page done to be ready to be edit

--- currency_exchange_converter/src/information.js
+++ currency_exchange_converter/src/information.js
@@ -5,12 +5,12 @@ const information =() =>{
             <h2>What we do</h2>
             <ol>
                 <li>
-                    Our role  is to convert any currency to another currency of your choice and all 
+                    Our role is to convert any currency to another currency of your choice and all 
                     conversion will be taken care of on our side.
                 </li>
             </ol>
             <br></br>
-            <h2>What We Provide</h2>
+            <h2>What We Do</h2>
             <ol>
                 <li>
                     Available Currencies: Canadian Dollar(CAD), Singapore Dollar(SGD), Malaysian Ringgit(MYR), United States Dollar(USD), 
@@ -19,7 +19,16 @@ const information =() =>{
                     Mexican Peso(MXN), Indonesian Rupiah(IDR), Taiwan New Dollar(TWD), Thai Baht(THB), and Vietnemese Dongs(VND).
                 </li>
                 <li>
-
+                    Finance Management: We provide efficient support for currency exchange services. 
+                </li>
+                <li>
+                    Transactions: We give our most swiftly transactions to access the exchanged funds.
+                </li>
+                <li>
+                    Service Assistance: We help to give the best assistance as much as we can to provide the best service.
+                </li>
+                <li>
+                    Safety: It will be guaranteed that the safety of all transactions will be calculated and transact to the right perosn.
                 </li>
             </ol>
         </div>
