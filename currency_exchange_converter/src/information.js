55b58f099c80ec0c823be03d2bf89a5ad8af41da
Noamakkusu <noahcgo@gmail.com>
2024-03-27 23:14:24 -0400

    installation of react-scripts

---
+++ currency_exchange_converter/src/information.js
@@ -0,0 +1,21 @@
+const information =() =>{
+    return(
+        <div>
+            <h1>About Currency Exchange Convertor</h1>
+            <h2>What we do</h2>
+            <ol>
+                <li>
+                    Our role  is to convert any currency to another currency of your choice and all 
+                    conversion will be taken care of on our side.
+                </li>
+            </ol>
+            <br></br>
+            <h2>What We Provide</h2>
+            <ol>
+                <li>
+                    Available Currencies:
+                </li>
+            </ol>
+        </div>
+    )
+}
