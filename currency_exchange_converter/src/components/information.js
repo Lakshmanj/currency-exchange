c3df646e2bc2089842f72ed16f7eb3964cb8ea6f
Lakshmanj <32884137+Lakshmanj@users.noreply.github.com>
2024-03-29 22:41:02 -0400

    changes for phase i with information.js

---
+++ currency_exchange_converter/src/components/information.js
@@ -0,0 +1,36 @@
+const information =() =>{
+    return(
+        <div>
+            <h1>About Currency Exchange Convertor</h1>
+            <h2>What we do</h2>
+            <ol>
+                <li>
+                    Our role is to convert any currency to another currency of your choice and all 
+                    conversion will be taken care of on our side.
+                </li>
+            </ol>
+            <br></br>
+            <h2>What We Do</h2>
+            <ol>
+                <li>
+                    Available Currencies: Canadian Dollar(CAD), Singapore Dollar(SGD), Malaysian Ringgit(MYR), United States Dollar(USD), 
+                    Euro(EUR), Australian Dollar(AUD), Japanese Yen(JPY), Chinese Yuan(CNH), Hong Kong Dollar(HKD), 
+                    Indian Rupee(INR), Danish Krone(DKK), British Pound Sterling(GBP), Russian Rouble(RUB), New Zealand Dollar(NZD),
+                    Mexican Peso(MXN), Indonesian Rupiah(IDR), Taiwan New Dollar(TWD), Thai Baht(THB), and Vietnemese Dongs(VND).
+                </li>
+                <li>
+                    Finance Management: We provide efficient support for currency exchange services. 
+                </li>
+                <li>
+                    Transactions: We give our most swiftly transactions to access the exchanged funds.
+                </li>
+                <li>
+                    Service Assistance: We help to give the best assistance as much as we can to provide the best service.
+                </li>
+                <li>
+                    Safety: It will be guaranteed that the safety of all transactions will be calculated and transact to the right perosn.
+                </li>
+            </ol>
+        </div>
+    )
+}
